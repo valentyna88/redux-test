35c00622d0f13d451a8d06e973dddfcf46364644
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-10-23 19:22:33 +0300

    Add TaskForm component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
 import AppBar from '../AppBar/AppBar';
 import Layout from '../Layout/Layout';
+import TaskForm from '../TaskForm/TaskForm';
 import './App.module.css';
 
 const App = () => {
   return (
     <Layout>
       <AppBar />
+      <TaskForm />
     </Layout>
   );
 };
--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,5 +1,24 @@
+import Button from '../Button/Button';
+import css from './TaskForm.module.css';
+
 const TaskForm = () => {
-  return;
+  const handleSubmit = event => {
+    event.preventDefault();
+    const form = event.target;
+    form.reset();
+  };
+
+  return (
+    <form className={css.form} onSubmit={handleSubmit}>
+      <input
+        className={css.field}
+        type="text"
+        name="text"
+        placeholder="Enter task text..."
+      />
+      <Button type="submit">Add task</Button>
+    </form>
+  );
 };
 
 export default TaskForm;
