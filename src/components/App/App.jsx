0e175bb1614804bd50c6de6796e400e2eb96caf8
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-10-23 22:38:41 +0300

    Update files

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,7 +2,6 @@ import AppBar from '../AppBar/AppBar';
 import Layout from '../Layout/Layout';
 import TaskForm from '../TaskForm/TaskForm';
 import TaskList from '../TaskList/TaskList';
-import './App.module.css';
 
 const App = () => {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './components/App/App';
 import 'modern-normalize';
 import './index.css';
-import App from './components/App/App';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
