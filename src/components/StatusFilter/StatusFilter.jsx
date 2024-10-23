09711d67fc527d811f45a1a70113f4019da101f4
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-10-24 01:28:03 +0300

    Update StatusFilter

--- src/components/StatusFilter/StatusFilter.jsx
+++ src/components/StatusFilter/StatusFilter.jsx
@@ -1,12 +1,16 @@
 import Button from '../Button/Button';
 import css from './StatusFilter.module.css';
+// 1. Імпортуємо хук
+import { useSelector } from 'react-redux';
 
 const StatusFilter = () => {
+  // 2. Отримуємо значення фільтра із стану Redux
+  const filter = useSelector(state => state.filters.status);
   return (
     <div className={css.wrapper}>
-      <Button>All</Button>
-      <Button>Active</Button>
-      <Button>Completed</Button>
+      <Button selected={filter === 'all'}>All</Button>
+      <Button selected={filter === 'active'}>Active</Button>
+      <Button selected={filter === 'completed'}>Completed</Button>
     </div>
   );
 };
