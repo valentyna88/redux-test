50c8237c1181cad48a31ba946b031f66dc947265
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-10-24 15:12:18 +0300

    Update TaskList

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,12 +1,32 @@
+// 1. Імпортуємо хук
+import { useSelector } from 'react-redux';
 import Task from '../Task/Task';
 import css from './TaskList.module.css';
 
+const getVisibleTasks = (tasks, statusFilter) => {
+  switch (statusFilter) {
+    case 'active':
+      return tasks.filter(task => !task.completed);
+    case 'completed':
+      return tasks.filter(task => task.completed);
+    default:
+      return tasks;
+  }
+};
+
 const TaskList = () => {
-  const tasks = [];
+  // 2. Отримуємо масив завдань із стану Redux
+  const tasks = useSelector(state => state.tasks.items);
+
+  // 3. Отримуємо значення фільтра із стану Redux
+  const statusFilter = useSelector(state => state.filter.status);
+
+  // 4. Обчислюємо масив завдань, які необхідно відображати в інтерфейсі
+  const visibleTasks = getVisibleTasks(tasks, statusFilter);
 
   return (
     <ul className={css.list}>
-      {tasks.map(task => {
+      {visibleTasks.map(task => {
         <li className={css.listItem} key={task.id}>
           <Task task={task} />
         </li>;
