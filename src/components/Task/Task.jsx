f395d150810bd18c6ddecc4b67086117355d7072
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-10-24 19:44:24 +0300

    Update Task

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,7 +1,20 @@
 import { MdClose } from 'react-icons/md';
 import css from './Task.module.css';
+// 1. Імпортуємо хук
+import { useDispatch } from 'react-redux';
+// 2. Імпортуємо фабрику екшену
+import { deleteTask } from '../../redux/actions';
 
 const Task = ({ task }) => {
+  // 3. Отримуємо посилання на функцію відправки екшенів
+  const dispatch = useDispatch();
+
+  // 4. Викликаємо фабрику екшену та передаємо ідентифікатор завдання
+  // 5. Відправляємо результат - екшен видалення завдання
+  const handleDelete = () => {
+    dispatch(deleteTask(task.id));
+  };
+
   return (
     <div className={css.wrapper}>
       <input
@@ -10,7 +23,7 @@ const Task = ({ task }) => {
         checked={task.completed}
       />
       <p className={css.text}>{task.text}</p>
-      <button className={css.btn}>
+      <button className={css.btn} onClick={handleDelete}>
         <MdClose size={24} />
       </button>
     </div>
