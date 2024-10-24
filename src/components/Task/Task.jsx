ef59e60d672a8ca42207320a5f58e9702bbfe74a
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-10-24 19:53:58 +0300

    Update Task component

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -3,7 +3,7 @@ import css from './Task.module.css';
 // 1. Імпортуємо хук
 import { useDispatch } from 'react-redux';
 // 2. Імпортуємо фабрику екшену
-import { deleteTask } from '../../redux/actions';
+import { deleteTask, toggleCompleted } from '../../redux/actions';
 
 const Task = ({ task }) => {
   // 3. Отримуємо посилання на функцію відправки екшенів
@@ -15,12 +15,19 @@ const Task = ({ task }) => {
     dispatch(deleteTask(task.id));
   };
 
+  // 6. Викликаємо фабрику екшену та передаємо ідентифікатор завдання
+  // 7. Відправляємо результат - екшен перемикання статусу завдання
+  const handleToggle = () => {
+    dispatch(toggleCompleted(task.id));
+  };
+
   return (
     <div className={css.wrapper}>
       <input
         className={css.checkbox}
         type="checkbox"
         checked={task.completed}
+        onChange={handleToggle}
       />
       <p className={css.text}>{task.text}</p>
       <button className={css.btn} onClick={handleDelete}>
