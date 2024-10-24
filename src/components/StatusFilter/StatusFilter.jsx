fa7516f456c1618c56d9f2505fdc2dbd5934ec0f
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-10-24 20:25:11 +0300

    Update StatusFilter

--- src/components/StatusFilter/StatusFilter.jsx
+++ src/components/StatusFilter/StatusFilter.jsx
@@ -1,17 +1,38 @@
 import Button from '../Button/Button';
 import css from './StatusFilter.module.css';
 // 1. Імпортуємо хук
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+// 2. Імпортуємо фабрику екшену
+import { setStatusFilter } from '../../redux/actions';
 
 const StatusFilter = () => {
-  // 2. Отримуємо значення фільтра із стану Redux
+  // 3. Отримуємо посилання на функцію відправки екшенів
+  const dispatch = useDispatch();
+  // // 2. Отримуємо значення фільтра із стану Redux
   const filter = useSelector(state => state.filters.status);
-
+  // 4. Викликаємо фабрику екшену та передаємо значення фільтра
+  // 5. Відправляємо результат - екшен зміни фільтра
+  const handleFilterChange = filter => dispatch(setStatusFilter(filter));
   return (
     <div className={css.wrapper}>
-      <Button selected={filter === 'all'}>All</Button>
-      <Button selected={filter === 'active'}>Active</Button>
-      <Button selected={filter === 'completed'}>Completed</Button>
+      <Button
+        selected={filter === 'all'}
+        onClick={() => handleFilterChange('all')}
+      >
+        All
+      </Button>
+      <Button
+        selected={filter === 'active'}
+        onClick={() => handleFilterChange('active')}
+      >
+        Active
+      </Button>
+      <Button
+        selected={filter === 'completed'}
+        onClick={() => handleFilterChange('completed')}
+      >
+        Completed
+      </Button>
     </div>
   );
 };
