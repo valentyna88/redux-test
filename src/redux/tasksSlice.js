a6f36ec953e40ab83fc3778d96aeb1003c0f7a59
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-10-26 01:09:08 +0300

    Update tasksSlice file

--- src/redux/tasksSlice.js
+++ src/redux/tasksSlice.js
@@ -1,50 +1,33 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const slice = createSlice({
-  // Ім'я слайсу
   name: 'tasks',
-  // Початковий стан редюсера слайсу
+
   initialState: {
-    items: [
-      { id: 0, text: 'Learn HTML and CSS', completed: true },
-      { id: 1, text: 'Get good at JavaScript', completed: true },
-      { id: 2, text: 'Master React', completed: false },
-      { id: 3, text: 'Discover Redux', completed: false },
-      { id: 4, text: 'Build amazing apps', completed: false },
-    ],
+    items: [],
   },
-  // Об'єкт case-редюсерів
+
   reducers: {
     addTask: (state, action) => {
-      return {
-        ...state,
-        items: [...state.items, action.payload],
-      };
+      // ✅ Immer замінить це на операцію оновлення
+      state.items.push(action.payload);
     },
     deleteTask: (state, action) => {
-      return {
-        ...state,
-        items: state.items.filter(task => task.id !== action.payload),
-      };
+      // ✅ Immer замінить це на операцію оновлення
+      state.items = state.items.filter(item => item.id !== action.payload);
     },
     toggleCompleted: (state, action) => {
-      return {
-        ...state,
-        items: state.items.map(task => {
-          if (task.id !== action.payload) {
-            return task;
-          }
-          return {
-            ...task,
-            completed: !task.completed,
-          };
-        }),
-      };
+      // ✅ Immer замінить це на операцію оновлення
+      for (const task of state.items) {
+        if (task.id === action.payload) {
+          task.completed = !task.completed;
+          break;
+        }
+      }
     },
   },
 });
-// Експортуємо фабрики екшенів
+
 export const { addTask, deleteTask, toggleCompleted } = slice.actions;
 
-// Експортуємо редюсер слайсу
 export default slice.reducer;
