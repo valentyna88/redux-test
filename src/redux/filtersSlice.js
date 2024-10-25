c02b1ab20ca1af322664475c34d73a8768afac2f
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-10-26 01:16:54 +0300

    Update filtersSlice file

--- src/redux/filtersSlice.js
+++ src/redux/filtersSlice.js
@@ -1,25 +1,20 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const slice = createSlice({
-  // Ім'я слайсу
   name: 'filters',
-  // Початковий стан редюсера слайсу
+
   initialState: {
     status: 'all',
   },
-  // Об'єкт case-редюсерів
+
   reducers: {
-    setStatusFilter: (state, action) => {
-      return {
-        ...state,
-        status: action.payload,
-      };
+    setStatusFilter(state, action) {
+      // ✅ Immer замінить це на операцію оновлення
+      state.status = action.payload;
     },
   },
 });
 
-// Експортуємо фабрики екшенів
 export const { setStatusFilter } = slice.actions;
 
-// Експортуємо редюсер слайсу
 export default slice.reducer;
